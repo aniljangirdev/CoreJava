f7b17f6fd1fe0cd246d878e8603e2e1a5507dabd
Anil Jangir <aniljangir212@gmail.com>
2024-06-19 06:51:35 +0530

    added more program

--- src/pack1/Main.java
+++ src/pack1/Main.java
@@ -2,93 +2,6 @@ package pack1;
 
 import java.util.LinkedList;
 
-class CustomHashMap<K, V> {
-    private static final int DEFAULT_CAPACITY = 16;
-    private final LinkedList[] buckets;
-    private int size;
-
-    public CustomHashMap() {
-        this(DEFAULT_CAPACITY);
-    }
-
-    public CustomHashMap(int capacity) {
-        buckets = new LinkedList[capacity];
-        for (int i = 0; i < capacity; i++) {
-            buckets[i] = new LinkedList<>();
-        }
-        size = 0;
-    }
-
-    public void put(K key, V value) {
-        if (key == null)
-            throw new IllegalArgumentException("Key cannot be null");
-
-        int index = getIndex(key);
-        LinkedList<Entry<K, V>> bucket = buckets[index];
-
-        for (Entry<K, V> entry : bucket) {
-            if (entry.key.equals(key)) {
-                entry.value = value; // Update the value if the key already exists
-                return;
-            }
-        }
-
-        bucket.add(new Entry<>(key, value));
-        size++;
-    }
-
-    public V get(K key) {
-        if (key == null)
-            throw new IllegalArgumentException("Key cannot be null");
-
-        int index = getIndex(key);
-        LinkedList<Entry<K, V>> bucket = buckets[index];
-
-        for (Entry<K, V> entry : bucket) {
-            if (entry.key.equals(key)) {
-                return entry.value;
-            }
-        }
-
-        return null; // Key not found
-    }
-
-    public void remove(K key) {
-        if (key == null)
-            throw new IllegalArgumentException("Key cannot be null");
-
-        int index = getIndex(key);
-        LinkedList<Entry<K, V>> bucket = buckets[index];
-
-        for (Entry<K, V> entry : bucket) {
-            if (entry.key.equals(key)) {
-                bucket.remove(entry);
-                size--;
-                return;
-            }
-        }
-    }
-
-    public int size() {
-        return size;
-    }
-
-    private int getIndex(K key) {
-        int hashCode = key.hashCode();
-        return Math.abs(hashCode % buckets.length);
-    }
-
-    private static class Entry<K, V> {
-        K key;
-        V value;
-
-        Entry(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         CustomHashMap<String, Integer> map = new CustomHashMap<>();
@@ -103,5 +16,10 @@ public class Main {
         map.remove("two");
         System.out.println("Size after removal: " + map.size());
         System.out.println("Value for 'two' after removal: " + map.get("two"));
+
+        User user = User
+                .builder()
+                .build();
+
     }
 }
---
+++ src/pack2/ShortestWordDistance.java
@@ -0,0 +1,28 @@
+package pack2;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ShortestWordDistance {
+
+    public static void main(String[] args) {
+        // find the distance of given two characters
+        //
+        String[] input = {"the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
+        String word1 = "fox";
+        String word2 = "dog";
+
+        // assume total length of the string
+        // as minimum distance
+        int min_dist = (input.length) + 1;
+
+        for (int i = 0; i < input.length; i++) {
+            if (input[i].equals(word1)) {
+
+
+            }
+        }
+
+
+    }
+}
